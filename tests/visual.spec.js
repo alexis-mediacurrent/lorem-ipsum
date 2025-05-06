58017894c926ecb623e6c74507751cca2d4a5bbb
Alexis <alexis.saransig@mediacurrent.com>
2025-05-06 17:08:25 -0500

    Commit for testing.

--- tests/visual.spec.js
+++ tests/visual.spec.js
@@ -2,7 +2,7 @@ const { test } = require('@playwright/test');
 const percySnapshot = require('@percy/playwright');
 
 test('Homepage visual test', async ({ page }) => {
-  await page.goto('https://lorem-ipsum.ddev.site');
+  await page.goto('http://lorem-ipsum.ddev.site');
   await page.waitForSelector('h2');
   await percySnapshot(page, 'Homepage');
 });
